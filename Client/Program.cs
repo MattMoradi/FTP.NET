c09949e915287271e9d8cc984fb1619729e51639
Cesium1289 <Carson.hansen@gmail.com>
2022-06-30 19:55:13 -0700

    Added connect function to allow for a conenction to a server

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FluentFTP;
+namespace ftp
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+           FtpClient client = new FtpClient();
+
+            if(Connect(ref client))
+                Console.WriteLine("Successfully connected to server!");
+            else
+                Console.WriteLine("ERROR: did not connect to server!");
+
+
+            Console.WriteLine(client.Credentials.UserName);
+            Console.WriteLine(client.Credentials.Password);
+            Console.WriteLine(client.Host + ":" + client.Port);
+        }
+
+        static public bool Connect(ref FtpClient client)
+        {
+            Console.WriteLine("\nEnter the server you want to join");
+            client.Host = Console.ReadLine();
+            Console.WriteLine("Enter the username:");
+            client.Credentials.UserName = Console.ReadLine();
+            Console.WriteLine("Enter the password:");
+            client.Credentials.Password = Console.ReadLine();
+
+            try
+            {
+                client.AutoConnect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+            return client.IsAuthenticated;
+        }
+    }
+}
