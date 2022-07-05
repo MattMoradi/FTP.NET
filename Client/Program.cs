df125642bd052f1270f696430f0eaa16f1758e5d
Peter Wells <unfinishedideas@gmail.com>
2022-07-05 16:44:23 -0700

    added basic listFiles function to Program.cs for now

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+void listFiles(String filepath)
+{
+    DirectoryInfo directory = new DirectoryInfo(@filepath);
+    DirectoryInfo[] sub_directories = directory.GetDirectories();
+    FileInfo[] files = directory.GetFiles();
+
+    // list directories
+    Console.WriteLine();
+    foreach (DirectoryInfo i in sub_directories)
+    {
+        Console.WriteLine("./{0}", i.Name);
+    }
+
+    // list files
+    Console.WriteLine();
+    foreach (FileInfo j in files)
+    {
+        Console.WriteLine("{0}", j.Name);
+    }
+}
+
+listFiles("C:\\dev");
