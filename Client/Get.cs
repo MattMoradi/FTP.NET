386b9a3d8aeb5aaa87662a77beba2251d35035dc
CristianTS599 <cristiant.salazar@microchip.com>
2022-07-12 13:16:03 -0700

    Contains code for the following features: Get multiple files / directory, change permission on remote, rename remote and local files. Code is untested (need valid FTP connection I havn't gotten it to work for me)

--- Client/Get.cs
+++ Client/Get.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using FluentFTP;
 
 namespace Client
@@ -8,17 +9,51 @@ namespace Client
         public static int File(ref FtpClient client, Commands.Get files)
         {
             Console.WriteLine("file: " + files.File);
-
+            
             if (files.Files.Count() > 1)
-                return MultipleFiles(files.Files);
+                return MultipleFiles(client, files.Files);
             return 0;
         }
 
-        public static int MultipleFiles(IEnumerable<string> files)
+        /// <summary>
+        /// Uses FluentFtp library method to find files on a remote server as a query executed against the passed int
+        /// ftpClient.
+        /// </summary>
+        /// <param name="ftpClient">Connection to remote ftp protocal server.</param>
+        /// <param name="remoteDirs">List of file directories.</param>
+        /// <param name="localDir">Local directory to save files.</param>
+        /// <returns>Number of files found.</returns>
+        /// <exception cref="Exception">Library method could fail.</exception>
+        public static int MultipleFiles(FtpClient ftpClient, IEnumerable<string> remoteDirs, string localDir = "")
         {
-            foreach (string file in files)
-                Console.WriteLine(file); // remove this
-            return 0;
+            try
+            {
+                return ftpClient.DownloadFiles(string.IsNullOrEmpty(localDir) ? Environment.CurrentDirectory : localDir, remoteDirs);
+            }
+            catch(Exception exc)
+            {
+                throw new Exception($"[Client.Get.MultipleFiles(client, IE<rDir>, lDir)] Failed to retrieve multiple files. Auto Exception Msg: {exc.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Uses the FluentFtp library to find and download a directory from the remote server to the local directory.
+        /// </summary>
+        /// <param name="ftpClient">Connection to remote ftp protocal server.</param>
+        /// <param name="remoteDir">Remote directory to grab.</param>
+        /// <param name="localDir">Local directory where items are saved.</param>
+        /// <returns>Collection of the items downloaded.</returns>
+        /// <exception cref="Exception">Library method could fail.</exception>
+        public static IEnumerable<FtpResult> Directory(FtpClient ftpClient, string remoteDir, string localDir = "")
+        {
+            try
+            {
+                return ftpClient.DownloadDirectory(string.IsNullOrEmpty(localDir) ? Environment.CurrentDirectory : localDir, remoteDir, FtpFolderSyncMode.Update);
+            }
+            catch(Exception exc)
+            {
+                throw new Exception($"[Client.Get.Directory(client, rDir, lDir)]: Failed to retrieve directory: {remoteDir}. Auto Generated Exception Msg: {exc.Message}");
+            }
         }
 
         public static int List(ref FtpClient client, Commands.List directory)
--- Client/Modify.cs
+++ Client/Modify.cs
@@ -12,12 +12,83 @@ namespace Client
 
         public static int Permissions(ref FtpClient client, Commands.Permissions file)
         {
-            return 0;
+            return Permissions(client);
+        }
+
+        /// <summary>
+        /// Changes the permissions of a specified remote file.
+        /// </summary>
+        /// <param name="client">Ftp server endpoint.</param>
+        /// <returns>0 if successful else -1.</returns>
+        /// <exception cref="Exception">Library methods could fail.</exception>
+        public static int Permissions(FtpClient client)
+        {
+            int result = -1;
+            var fPath = string.Empty;
+            try
+            {
+                // Permission level defaults to read only
+                int permLevel = 4;
+                Console.WriteLine("Enter File Path: ");
+                fPath = Console.ReadLine() ?? String.Empty;
+
+                Console.WriteLine("Enter Permission in CHMOD Format: ");
+                int.TryParse(Console.ReadLine() ?? String.Empty, out permLevel);
+
+                // Invoke library method to execute permission change.
+                client.SetFilePermissions(fPath, permLevel);
+                result = 0;
+            }
+            catch (Exception exc)
+            {
+                throw new Exception($"[Client.Modify.Permissions(client)] Failed to Change Permissions of {fPath}. Auto Exception Message: {exc.Message}");
+            }
+            return result;
         }
 
         public static int Rename(ref FtpClient client, Commands.Rename file)
         {
-            return 0;
+            return Rename(client);
         }
+
+        /// <summary>
+        /// Will rename a file on local or remote directory depending on where it exists.
+        /// </summary>
+        /// <param name="client">ftpClient on which potential file to rename exists.</param>
+        /// <returns>0 if the file was renamed successfully, -1 if it failed.</returns>
+        /// <exception cref="Exception">Could be library call critical errors or System.IO.Directory failure.</exception>
+        public static int Rename(FtpClient client)
+        {
+            int result = -1;
+            string oldName = string.Empty;
+            string newName = string.Empty;
+            try
+            {
+                Console.WriteLine("Enter Old File Path: ");
+                oldName = Console.ReadLine() ?? String.Empty;
+
+                Console.WriteLine("Enter File Path Ending With Rename Value: ");
+                newName = Console.ReadLine() ?? String.Empty;
+
+                // Check if the file exists on the remote server.
+                if (client.FileExists(oldName))
+                {
+                    // rename remote file
+                    result = client.MoveFile(oldName, newName) ? 0 : -1;
+                }
+                // check if local file exists with given name
+                else if (Directory.Exists(oldName))
+                {
+                    Directory.Move(oldName, newName);
+                    result = 0;
+                }
+            }
+            catch(Exception exc)
+            {
+                throw new Exception($"[Client.Modify.Rename(client, file, oNm, nNm)] Failed to rename file {oldName}. Auto Exception Msg: {exc.Message}");
+            }
+
+            return result;
+        }    
     }
 }
