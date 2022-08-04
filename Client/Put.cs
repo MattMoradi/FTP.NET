54110613bf002eb6b17d1e5aa9a8c5c755c47985
Peter Wells <unfinishedideas@gmail.com>
2022-08-04 15:56:28 -0700

    basic file put complete!

--- Client/Put.cs
+++ Client/Put.cs
@@ -1,17 +1,51 @@
 ﻿using System;
 using FluentFTP;
+using ShellProgressBar;
 namespace Client
 {
     public static class Put
     {
-        public static int File(ref FtpClient client, Commands.Put file)
+        public static int File(ref FtpClient client, Commands.Put file, in Program.FilePath path)
         {
             Console.WriteLine("file: " + file.File);
 
-            if (file.Files.Count() > 1)
-                return MultipleFiles(file.Files);
+                if (!client.IsAuthenticated)
+                {
+                    throw new Exception("Error: No connection to remote server!");
+                }
+                if (file.Files.Count() > 1)
+                    return MultipleFiles(file.Files);
+
+                var options = new ProgressBarOptions
+                {
+                    ForegroundColor = ConsoleColor.Yellow,
+                    ForegroundColorDone = ConsoleColor.DarkGreen,
+                    BackgroundColor = ConsoleColor.DarkGray,
+                    BackgroundCharacter = '\u2593'
+                };
+
+                using var progressBar = new ProgressBar(10000, "uploaded", options);
+
+                Action<FtpProgress> progress = delegate (FtpProgress download)
+                {
+                    var progress = progressBar.AsProgress<double>();
+                    progress.Report(download.Progress / 100);
+                };
+
+            try
+            {
+                string fullRemotePath = path.Remote + Path.GetFileName(file.File);
+                Console.WriteLine(fullRemotePath);
+                client.UploadFile(@file.File, @fullRemotePath, FtpRemoteExists.Overwrite, true, FtpVerify.OnlyChecksum, progress);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.InnerException);
+                return -1;
+            }
             return 0;
         }
+
 
         public static int MultipleFiles(IEnumerable<string> files)
         {
