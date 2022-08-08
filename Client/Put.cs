b3fdf097c068e4c7320c38bbe065d3e66c0f2083
travismcgowan <72109127+travismcgowan@users.noreply.github.com>
2022-08-08 10:24:20 -0700

    Just merging, manually, because I'm not terribly familiar with managing merge conflicts in Git

--- Client/Put.cs
+++ Client/Put.cs
@@ -129,6 +129,92 @@ namespace Client
 
 		public static int Copy(ref FtpClient client, Commands.Copy file)
 		{
+			string sourcedir;
+			string targetdir;
+
+			if (!client.IsAuthenticated)
+			{
+				Console.WriteLine("ERROR: Not connected to remote server!\n");
+				return -1;
+			}
+
+			//Check whether there are two arguments, if there are not, return error message
+			if (file.Directories.Count() != 2)
+			{
+				Console.WriteLine("You must provide two arguments for the copy directory command.");
+				return -1;
+			}
+			else
+			{
+				sourcedir = file.Directories.First();
+				targetdir = file.Directories.Last();
+			}
+
+
+			//Check whether the source dir exists. If it does not, return an error message
+			if (!client.DirectoryExists(file.Directories.First()))
+			{
+				Console.WriteLine("The source directory does not exist.");
+				return -1;
+			}
+
+			//Check whether the destination dir exists. If it does ask whether they would like to delete the contents and replace w/ the source dir and it's contents
+			if (client.DirectoryExists(file.Directories.Last()))
+			{
+				Console.WriteLine("The destination directory already exists. Would you like to overwrite it? (Y/N)");
+
+				var response = Console.ReadLine();
+
+				if (String.IsNullOrEmpty(response))
+				{
+					response = "No";
+				}
+
+				if ((String.Equals(response, "y")) || (String.Equals(response, "Y")) || (String.Equals(response, "Yes")) || (String.Equals(response, "YES")) || (String.Equals(response, "yes")))
+				{
+					client.DeleteDirectory(file.Directories.Last());
+				}
+				else
+				{
+					return 0;
+				}
+			}
+
+			try
+			{
+				client.CreateDirectory(targetdir);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("The target directory name provided is invalid.");
+				Console.WriteLine("Error Message: " + ex.Message);
+				return -1;
+			}
+
+			//ALL CHECKS SHOULD BE PERFORMED AT THIS POINT SO OK TO GO FORWARD WITH THE DL/UL/DEL
+
+			string localtempdir = "C:\\fluentftp_tempdir";
+
+			client.DownloadDirectory(localtempdir, sourcedir, FtpFolderSyncMode.Update);
+
+			string[] dirs = Directory.GetDirectories(localtempdir);
+
+			client.CreateDirectory(targetdir);
+
+			foreach (string dir in dirs)
+			{
+				client.UploadDirectory(dir, targetdir, FtpFolderSyncMode.Update);
+			}
+
+			string[] files = Directory.GetFiles(localtempdir);
+
+			foreach (string f in files)
+			{
+				client.UploadFile(f, targetdir);
+			}
+
+			Directory.Delete(localtempdir, true);
+
 			return 0;
 		}
 	}
