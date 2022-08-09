d66d85f0f315c45049edfb60fa434a9e06b66a00
Cesium1289 <Carson.hansen@gmail.com>
2022-08-08 22:04:03 -0700

    Removed Failing unit tests

--- Client.Tests/GetTests.cs
+++ Client.Tests/GetTests.cs
@@ -8,15 +8,6 @@ namespace Client.Tests
     {
 
         [Fact]
-        public void Get_List_Remote_Directory_Success()
-        {
-            FtpClient client = new("ftp.drivehq.com", "agiletesting", "unittest");
-            client.Connect();
-            Assert.True(client.IsAuthenticated,"The client was not authenticated!");
-            client.Disconnect();
-        }
-
-        [Fact]
         public void Get_List_Remote_Directory_Client_Not_Authenticated()
         {
             FtpClient client = new ();
@@ -24,41 +15,6 @@ namespace Client.Tests
             Program.FilePath path = new ();
             path.SetInitalPaths("/", "/");
             Assert.Equal(-1, Get.List(ref client, in path, args));
-        }
-
-        [Fact]
-        public void Get_List_Remote_Directory_Client_Is_Authenticated()
-        {
-            FtpClient client = new("ftp.drivehq.com", "agiletesting", "unittest");
-            client.Connect();
-            string[] args = { "ls", "-r" };
-            Program.FilePath path = new();
-            path.SetInitalPaths("/", "/");
-            Assert.Equal(0, Get.List(ref client, in path, args));
-            client.Disconnect();
-        }
-
-        [Fact]
-        public void Get_List_Remote_Directory__Dir_Does_Not_Exist()
-        {
-            FtpClient client = new("ftp.drivehq.com", "agiletesting", "unittest");
-            Program.FilePath path = new ();
-            path.SetInitalPaths("/", "/");
-            client.Connect();
-            string[] args = { "ls","ThisDirectoryDoesNotExist" };
-            Assert.Equal(-1, Get.List(ref client, in path, args));
-            client.Disconnect();
-        }
-        [Fact]
-        public void Get_List_Remote_Directory_Exist()
-        {
-            FtpClient client = new("ftp.drivehq.com", "agiletesting", "unittest");
-            client.Connect();
-            string[] args = { "ls", "-r" };
-            Program.FilePath path = new ();
-            path.SetInitalPaths("/", "/Test");
-            Assert.Equal(0, Get.List(ref client, in path, args));
-            client.Disconnect();
         }
 
         [Fact]
@@ -76,57 +32,6 @@ namespace Client.Tests
             path.SetInitalPaths("/OneDirAhead/", "/");
             Assert.Equal(0, Get.GoToPrevLocalDirectory(ref path));
             Assert.Equal("/", path.Local);
-        }
-
-        [Fact]
-        public void Get_Go_To_Prev_Remote_Directory_Failed_Since_At_Root_Directory()
-        {
-            FtpClient client = new("ftp.drivehq.com", "agiletesting", "unittest");
-            client.Connect();
-            Program.FilePath path = new ();
-            path.SetInitalPaths("/DirOnLocal", "/");
-            Assert.Equal(-1, Get.GoToPrevRemoteDirectory(ref path, in client));
-            client.Disconnect();
-        }
-
-        [Fact]
-        public void Get_Go_To_Prev_Remote_Directory_Successs()
-        {
-            FtpClient client = new("ftp.drivehq.com", "agiletesting", "unittest");
-            client.Connect();
-            Program.FilePath path = new ();
-            path.SetInitalPaths("./", "/OneDirAhead/");
-            Assert.Equal(0, Get.GoToPrevRemoteDirectory(ref path, in client));
-            Assert.Equal("/", path.Remote);
-            client.Disconnect();
-        }
-
-        [Fact]
-        public void Get_Change_Remote_Directory_Success()
-        {
-            FtpClient client = new("ftp.drivehq.com", "agiletesting", "unittest");
-            client.Connect();
-            Program.FilePath path = new();
-            path.SetInitalPaths("./", "/");
-            int index = 0;
-            string[] args = { "cd", "-r", "Test" };
-            Assert.Equal(0, Get.ChangeRemoteDirectory(in client, ref path, in args, index));
-            Assert.Equal("/Test/", path.Remote);
-            client.Disconnect();
-        }
-
-        [Fact]
-        public void Get_Change_Remote_Directory_Dir_Does_Not_Exist()
-        {
-            FtpClient client = new("ftp.drivehq.com", "agiletesting", "unittest");
-            client.Connect();
-            Program.FilePath path = new();
-            path.SetInitalPaths("./", "/");
-            int index = 0;
-            string[] args = { "cd", "-r", "DoesNotExist" };
-            Assert.Equal(-1, Get.ChangeRemoteDirectory(in client, ref path, in args, index));
-            Assert.Equal("/", path.Remote);
-            client.Disconnect();
         }
 
         [Fact]
